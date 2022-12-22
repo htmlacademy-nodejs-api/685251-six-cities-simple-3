6f4a523a267712b3959b68722bb1a86324cbaa01
vokiss <vokisskotov@gmail.com>
2022-12-22 22:58:16 +0300

    fix

--- src/cli-command/version-command.ts
+++ src/cli-command/version-command.ts
@@ -5,7 +5,7 @@ export default class VersionCommand implements CliCommandInterface {
   public readonly name = '--version';
 
   private readVersion(): string {
-    const contentPageJSON = readFileSync('./package.json', 'utf-8')
+    const contentPageJSON = readFileSync('./package.json', 'utf-8');
     const content = JSON.parse(contentPageJSON);
     return content.version;
   }
